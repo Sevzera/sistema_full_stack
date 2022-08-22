b7d65d317e3ac4cbb81177d5c6e1c71c55619031
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-08-22 19:24:28 -0300

    permissoes backend

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,10 +10,7 @@ export const server = async () => {
 	server.use(function (req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "*");
-		res.header(
-			"Access-Control-Allow-Methods",
-			"GET, POST, PUT, DELETE, OPTIONS"
-		);
+		res.header("Access-Control-Allow-Methods", "*");
 		next();
 	});
 	server.use(express.json());
